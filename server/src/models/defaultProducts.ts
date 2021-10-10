23d7a395e098399a2d98b4f354ae7efbc057a4d8
jgrindall <john.grindall@gmail.com>
2021-10-10 15:50:51 +0100

    add some randomness

--- server/src/models/defaultProducts.ts
+++ server/src/models/defaultProducts.ts
@@ -3,8 +3,8 @@ import {Product} from "../types";
 const defaultProducts:Product[] = [
     {
         id: 1,
-        title: "Product 1",
-        description: "1: Lorem ipsum dolor sit amet",
+        title: "Product 1 - ",
+        description: "1: Lorem ipsum dolor sit amet" + Math.floor(Math.random()*1000000),
         fullDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
         image:"https://johnlewis.scene7.com/is/image/JohnLewis/003839248?$rsp-pdp-port-640$",
         price: 30,
@@ -13,7 +13,7 @@ const defaultProducts:Product[] = [
     {
         id: 2,
         title: "Product 2",
-        description: "2: Lorem ipsum dolor sit amet",
+        description: "2: Lorem ipsum dolor sit amet" + Math.floor(Math.random()*1000000),
         fullDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
         image:"https://johnlewis.scene7.com/is/image/JohnLewis/005610645?$rsp-pdp-port-640$",
         price: 20,
@@ -22,7 +22,7 @@ const defaultProducts:Product[] = [
     {
         id: 3,
         title: "Product 3",
-        description: "3: Lorem ipsum dolor sit amet",
+        description: "3: Lorem ipsum dolor sit amet" + Math.floor(Math.random()*1000000),
         fullDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
         image:"https://johnlewis.scene7.com/is/image/JohnLewis/004045156?$rsp-pdp-port-640$",
         price: 10,
