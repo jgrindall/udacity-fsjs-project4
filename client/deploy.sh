da1f1c630d31fb93c71006b38099d2b8675168fe
jgrindall <john.grindall@gmail.com>
2021-10-11 18:31:52 +0100

    add user?

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+aws configure --profile "deploy-cli"
+
 aws s3 rm s3://jgrindalludacity --recursive --profile deploy-cli
 
 aws s3 cp --recursive --acl public-read ./dist/myApp s3://jgrindalludacity/ --profile deploy-cli
