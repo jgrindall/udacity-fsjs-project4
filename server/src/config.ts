18e16e223eaab12e28a916f92ab331bea7ca4265
jgrindall <john.grindall@gmail.com>
2021-10-15 17:43:01 +0100

    typo

--- server/src/config.ts
+++ server/src/config.ts
@@ -10,7 +10,7 @@ if(!process.env.POSTGRES_DB){
 
 export default {
     JWT_TOKEN_SECRET: process.env.JWT_TOKEN_SECRET as string,
-    SALT_ROUNDS: process.env.SALT_ROUNDS ? parseInt(process.env.SALT_ROUNDS as string),
+    SALT_ROUNDS: parseInt(process.env.SALT_ROUNDS as string),
     BCRYPT_PASSWORD: process.env.BCRYPT_PASSWORD as string,
     POSTGRES_HOST: process.env.POSTGRES_HOST as string,
     POSTGRES_DB: process.env.POSTGRES_DB as string,
