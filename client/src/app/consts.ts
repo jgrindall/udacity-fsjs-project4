3bf8fd3bd970cc3de40b09c0edad0da9ec77c086
jgrindall <john.grindall@gmail.com>
2021-10-15 17:43:22 +0100

    console.log?

--- client/src/app/consts.ts
+++ client/src/app/consts.ts
@@ -1,6 +1,8 @@
 
 //export const API:string = 'http://localhost:3000/api';
 
+console.log(window.location.href);
+
 export const API:string = 'http://jgrindalludacity-dev.us-west-2.elasticbeanstalk.com/api';
 
 export const HEADERS:Record<string, string> = {
