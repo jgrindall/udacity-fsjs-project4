1286dc9f8d3d3b38083579d7426396ac0710936a
jgrindall <john.grindall@gmail.com>
2021-10-17 14:08:47 +0100

    api url

--- client/src/app/consts.ts
+++ client/src/app/consts.ts
@@ -1,6 +1,9 @@
 const env = window.location.href.indexOf("amazonaws") >= 0 ? "PROD" : "DEV";
 
-export const API:string = (env == "PROD" ? 'http://jgrindalludacity-dev.us-west-2.elasticbeanstalk.com/api' : "http://localhost:3000/api";
+const API_PROD = 'http://jgrindalludacity-dev.us-west-2.elasticbeanstalk.com/api';
+const API_DEV = "http://localhost:3000/api";
+
+export const API:string = (env == "PROD" ?  API_PROD : API_DEV);
 
 export const HEADERS:Record<string, string> = {
   'Content-Type': 'application/json',
