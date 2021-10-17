78ec85a855f6b447714a315c2d9d1cbc58398409
jgrindall <john.grindall@gmail.com>
2021-10-17 16:20:13 +0100

    typo

--- server/healthcheck.sh
+++ server/healthcheck.sh
@@ -2,7 +2,7 @@
 STATUS=$( curl -s -o /dev/null -I -w "%{http_code}" http://jgrindalludacity-dev.us-west-2.elasticbeanstalk.com/)
   echo $STATUS
 if [ $STATUS -eq 200 ]; then
-    exit 1
-else
     exit 0
+else
+    exit 1
 fi
